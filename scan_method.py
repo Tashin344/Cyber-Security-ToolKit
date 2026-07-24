00d518169cefa58f58425bd061a69040166544b3
Tashin <tashinalam344@gmail.com>
2026-07-25 01:02:32 +0530

    V0.3: Added socket timeout

--- scan_method.py
+++ scan_method.py
@@ -4,6 +4,7 @@ def scan_method(ip_input, start_port, end_port):
     
     for i in range(start_port, end_port + 1):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
         try:
             s.connect((ip_input, i))
             print(f"{ip_input}:\n{i} OPEN")
